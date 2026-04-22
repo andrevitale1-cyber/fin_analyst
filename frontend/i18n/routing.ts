1e26b41e348a4192e68a28730c83ed46bffe48a9
andrevitale1-cyber <andrevitale1@gmail.com>
2026-04-22 19:56:18 -0300

    feat: initialize next-intl routing configuration with English and Portuguese support

--- frontend/i18n/routing.ts
+++ frontend/i18n/routing.ts
@@ -3,8 +3,7 @@ import {createNavigation} from 'next-intl/navigation';
 
 export const routing = defineRouting({
   locales: ['en', 'pt'],
-  defaultLocale: 'pt',
-  localeDetection: false
+  defaultLocale: 'pt'
 });
 
 export const {Link, redirect, usePathname, useRouter, getPathname} =
